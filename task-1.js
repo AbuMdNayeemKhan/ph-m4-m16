09212b9555edba9fc9601acfa2b4884f69f18dfa
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-01-26 12:34:37 +0600

    learning how to return

---
+++ task-1.js
@@ -0,0 +1,18 @@
+// Task-1
+// You went to the supermarket to buy some oranges and apples. Calculate how much money the shopkeeper will return.
+
+// Input:
+// The first line of the input is the taka you have. The second line is the cost of 1 kg of oranges and 1 kg of apples.
+
+var taka = 1000;
+var apple = 400;
+var orange = 300;
+// Output:
+// Print the result.
+var totalPrice = apple + orange;
+
+// Sample Input:
+console.log("Total Taka: " + taka);
+console.log("Total Price: " + totalPrice);
+// Sample Output:
+console.log("Return Price: " + (taka - totalPrice));
